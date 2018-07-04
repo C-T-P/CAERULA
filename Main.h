cbaad37062633b40e055d7e2487c3800bcb5e534
Christian Preuss <CPreuss1@gmx.de>
2018-07-04 12:09:10 +0200

    add printing of matrices to files

--- Main.h
+++ Main.h
@@ -3,5 +3,6 @@
 
 void read_in_process(string filename, diagram& process, vector<string>& basis_str);
 colour_term decompose_terms(string& input, diagram process);
+string generate_out_filename(diagram process);
 
 #endif
