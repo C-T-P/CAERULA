79e1b81806b9fdd8c659d03f06ebee6c2d32c2db
Christian Preuss <CPreuss1@gmx.de>
2018-07-04 12:08:52 +0200

    remove printing of matrices to files

--- BASIS.h
+++ BASIS.h
@@ -5,6 +5,5 @@ vector<vector<complex<double>>> calc_soft_matrix(vector<colour_term> basis, int 
 vector<vector<complex<double>>> calc_inv_soft_matrix(vector<vector<complex<double>>> soft_matrix);
 vector<vector<complex<double>>> calc_colour_change_matrix(vector<colour_term> basis, vector<vector<complex<double>>> soft_matrix, diagram process, unsigned int lno1, unsigned int lno2, int NC_order, bool inv_mult);
 colour_term make_internal(diagram process, colour_term expr);
-void save_colour_to_file(vector<vector<vector<complex<double>>>> colour_change_matrices, vector<vector<complex<double>>> soft_matrix, diagram process);
 
 #endif
